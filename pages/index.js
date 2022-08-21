ce5b1b6383f6f8b11d8ba1b317757a30989653d1
pablomarcel <pablomarcel@outlook.com>
2022-08-20 17:47:36 -0700

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <Link href="/posts/">
-          <a className={styles.btn}>See Post Listing</a>
+          <a className={styles.btn}>See Posts Listing</a>
         </Link>
       </div>
     </>
